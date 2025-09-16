8bb1dc44b4e403bd82652ad4104168ea17ca223e
Rendszergazda <rendszergazda@blathy.info>
2025-09-16 11:36:56 +0200

    0916 javítás

--- 0912/Feladatsor/Feladatsor/Program.cs
+++ 0912/Feladatsor/Feladatsor/Program.cs
@@ -5,73 +5,69 @@ namespace Feladatsor
 {
     internal class Program
     {
-        static void Feladat1()
+        static void F1()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world!");
         }
-        static void Feladat2()
+        static void F2()
         {
             Console.Write("Kérlek, add meg a neved: ");
-            string be = Console.ReadLine()!;
+            string input = Console.ReadLine()!;
 
-            Console.WriteLine($"Szióka, {be}");
+            Console.WriteLine($"Szióka {input}!");
         }
-        static void Feladat3()
+        static void F3()
         {
-            Console.Write("Adj egy számot plíz: ");
-            string be = Console.ReadLine()!;
+            Console.Write("Írj be egy számot: ");
             int? num = null;
             do
             {
+                string input = Console.ReadLine()!;
                 try
                 {
-                    num = int.Parse(be);
+                    num = int.Parse(input);
                     Console.WriteLine(num * (long)2);
-                    return;
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Ez nem lesz így jó...");
+                    Console.WriteLine("Kérlek egy egész számot adj, ne betűt. Se tört számot, azt sem szeretjük.");
                 }
                 catch (OverflowException)
                 {
-                    Console.WriteLine("Ekkora szám nem létezik.");
+                    Console.WriteLine("Ekkora szám szerény véleményem szerint nem létezik.");
                 }
-                Console.Write("Próbáld újra xdddddd: ");
-                be = Console.ReadLine();
             }
             while (num is null);
         }
-        static void Feladat4()
+        static void F4()
         {
-            Console.Write("ADjá egész számt! ");
-            int be1 = int.Parse(Console.ReadLine()!);
+            int input1 = AskForInt("Kérek egy számot: ");
 
-            Console.Write("Adj még egy számot!! ");
-            int be2 = int.Parse(Console.ReadLine()!);
+            int input2 = AskForInt("Mégegyet: ");
 
-            Console.WriteLine($"A két szám összege és különbsége {be1 + be2}, {be1 - be2}");
-            Console.Write($"Szorzata: {be1 * be2}");
-            if (be1 != 0 && be2 != 0)
+            Console.WriteLine($"A két szám összege és különbsége {input1 + input2}, {input1 - input2}");
+            Console.Write($"Szorzata: {input1 * input2}");
+            if (input1 != 0 && input2 != 0)
             {
-                Console.Write($" és hányadosa... {be1 / be2}");
+                Console.Write($" és hányadosa... {input1 / input2}");
             }
         }
-        static void Feladat5()
+        static void F5()
         {
+            /*
             try
             {
                 Console.Write("Egy számot ami egész és nem betű: ");
-                int be1 = int.Parse(Console.ReadLine()!);
+                int input1 = int.Parse(Console.ReadLine()!);
 
                 Console.Write("Mégegy számot ami nem betű és egész pls különben meghalsz: ");
-                int be2 = int.Parse(Console.ReadLine()!);
+                int input2 = int.Parse(Console.ReadLine()!);
 
-                if (be1 > be2)
+                if (input1 > input2)
                 {
-                    Console.WriteLine("Első ngyobb!!");
+                    Console.WriteLine("Első nagyobb!!");
                 }
-                else if (be1 < be2)
+                else if (input1 < input2)
                 {
 
                     Console.WriteLine("Második nagyobb!!!!");
@@ -87,12 +83,30 @@ namespace Feladatsor
             {
                 Console.WriteLine("megverlek");
             }
+            */
+
+            int input1 = AskForInt("Adj meg egy számot: ");
+            int input2 = AskForInt("Adj meg mégegy számot: ");
+
+            if (input1 > input2)
+            {
+                Console.WriteLine("Az első megadott szám nagyobb.");
+            }
+            else if (input1 < input2)
+            {
+
+                Console.WriteLine("A második megadott szám nagyobb.");
+            }
+            else
+            {
+                Console.WriteLine("A két szám egyenlő.");
+            }
         }
-        static void Feladat6()
+        static void F6()
         {
-            int num1 = Beker("Kérek számot: ");
-            int num2 = Beker("Mégegyet: ");
-            int num3 = Beker("És mégegyet...: ");
+            int num1 = AskForInt("Kérek számot: ");
+            int num2 = AskForInt("Mégegyet: ");
+            int num3 = AskForInt("És mégegyet...: ");
 
 
             if (num1 < num2 && num1 < num3)
@@ -110,14 +124,14 @@ namespace Feladatsor
 
         }
 
-        static void Feladat7()
+        static void F7()
         {
-            int a = Beker("Kérem a háromszög egyik oldalát: ");
-            int b = Beker("Másikat: ");    
-            int c = Beker("És a harmadikat: ");
+            int a = AskForInt("Kérem a háromszög egyik oldalát: ");
+            int b = AskForInt("Másikat: ");
+            int c = AskForInt("És a harmadikat: ");
 
 
-            if(a + b > c && a + c > b && b + c > a)
+            if (a + b > c && a + c > b && b + c > a)
             {
                 Console.WriteLine("Ebből nagyon gyönyörű háromszöget szerkeszthetsz :)");
             }
@@ -128,43 +142,44 @@ namespace Feladatsor
 
         }
 
-        static void Feladat8()
+        static void F8()
         {
-            int num1 = Beker("Számot pls!!! ");
-            int num2 = Beker("AAAAAAAAA!! ");
+            int num1 = AskForInt("Add meg az egyik számot: ");
+            int num2 = AskForInt("Add meg a másik számot: ");
 
-            Console.WriteLine($"A számtani és mértani?? közepe: {(double)(num1 + num2) / 2}");
+            Console.WriteLine($"A számtani és mértani?? közepe: {(num1 + num2) / 2.0}");
         }
 
-        static void Feladat9() 
+        static void F9()
         {
-            int a = Beker("Add meg az egyenlet egyik együtthatóját: ");
-            int b = Beker("Add meg az egyenlet másik együtthatóját: ");
-            int c = Beker("Add meg az egyenlet harmadik együtthatóját: ");
+            int a = AskForInt("Add meg az egyenlet egyik együtthatóját: ");
+            int b = AskForInt("Add meg az egyenlet másik együtthatóját: ");
+            int c = AskForInt("Add meg az egyenlet harmadik együtthatóját: ");
 
-            double dis = Math.Pow(b, 2)-4*a*c;
+            double dis = Math.Pow(b, 2) - 4 * a * c;
 
-            if (dis >= 0) {
+            if (dis >= 0)
+            {
                 Console.WriteLine("Az egyenletnek van megoldása.");
             }
             else
             {
-                Console.WriteLine("Az egyenletnek nincs megoldása.");
+                Console.WriteLine("Az egyenletnek nincs megoldása. Szerintem.");
             }
         }
-        static void Feladat10()
+        static void F10()
         {
-            int a = Beker("Add meg az egyenlet egyik együtthatóját: ");
-            int b = Beker("Add meg az egyenlet másik együtthatóját: ");
-            int c = Beker("Add meg az egyenlet harmadik együtthatóját: ");
+            int a = AskForInt("Add meg az egyenlet egyik együtthatóját: ");
+            int b = AskForInt("Add meg az egyenlet másik együtthatóját: ");
+            int c = AskForInt("Add meg az egyenlet harmadik együtthatóját: ");
 
             double dis = Math.Pow(b, 2) - 4 * a * c;
 
-            double res1 = (b + Math.Sqrt(dis))/2;
+            double res1 = (b + Math.Sqrt(dis)) / 2;
             double res2 = (b - Math.Sqrt(dis)) / 2;
 
             if (dis >= 0)
-            {   
+            {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Egyik megoldás {res1}, másik megoldás {res2}");
                 Console.ForegroundColor = ConsoleColor.White;
@@ -175,99 +190,118 @@ namespace Feladatsor
             }
         }
 
-        static void Feladat11()
+        static void F11()
         {
-            int a = Beker("Add meg a háromszög egyik befogóját: ");
-            int b = Beker("Add meg a 3szög másik befogóját!! ");
+            int a = AskForInt("Add meg a háromszög egyik befogóját: ");
+            int b = AskForInt("Add meg a 3szög másik befogóját!! ");
 
             double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-            
+
             Console.WriteLine($"  -  A 3szög átfogója {c:F2}.");
         }
 
-        static void Feladat12()
+        static void F12()
         {
-            int a = Beker("Egyik él: ");
-            int b = Beker("Másik él: ");
-            int c = Beker("Harmadik él: ");
+            int a = AskForInt("Egyik él: ");
+            int b = AskForInt("Másik él: ");
+            int c = AskForInt("Harmadik él: ");
 
-            int sur = (a * b)*2 + (b * c)*2 + (a * c)*2;
+            int sur = (a * b) * 2 + (b * c) * 2 + (a * c) * 2;
             int vol = a * b * c;
 
             Console.WriteLine($"A valami mérete: {sur} és {vol}");
         }
 
-        static void Feladat13()
+        static void F13()
         {
-            double radius = DoubleBeker("Add meg a kör átmérőjét: ");
+            double radius = AskForDouble("Add meg a kör átmérőjét: ");
             double r = radius / 2;
 
-            Console.WriteLine($"A kör kerülete: {2*r*Math.PI:F2}, területe: {Math.Pow(r, 2)*Math.PI:F2}");         
+            Console.WriteLine($"A kör kerülete: {2 * r * Math.PI:F2}, területe: {Math.Pow(r, 2) * Math.PI:F2}");
         }
-        static void Feladat14()
+        static void F14()
         {
-            double r = DoubleBeker("Add meg a sugarat: ");
-            double a = DoubleBeker("Add meg a szöget: ");
+            double r = AskForDouble("Add meg a sugarat: ");
+            double a = AskForDouble("Add meg a szöget: ");
 
-            double i = ((2*r * Math.PI) / 360) * a;
+            double i = ((2 * r * Math.PI) / 360) * a;
 
             Console.WriteLine($"A körcikk területe {(r * i) / 2:F2}, ív hossza {i:F2}");
         }
-
-
-        static int Beker(string text)
+        static void F15() 
         {
-            Console.Write(text);
-            string inp = Console.ReadLine()!;
+            int input;
+            do
+            {
+                input = AskForInt("Kérlek, adj meg egy pozitív egész számot: ");
+                for (global::System.Int32 i = 0; i < input; i++)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            while (input <= 0);
+        } 
 
+        static int AskForInt(string text)
+        {
             int? num = null;
             do
             {
+                Console.Write(text);
+                string inp = Console.ReadLine()!;
                 try
                 {
                     num = int.Parse(inp);
-                    return (int)num;
                 }
-                catch (Exception)
+                catch (OverflowException)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Valami nem jó itt, gondold át!");
+                    Console.WriteLine("Nagy szám ez, nem tudok vele mit kezdeni...");
                 }
-                Console.ForegroundColor= ConsoleColor.White;
-                Console.Write(text);
-                inp = Console.ReadLine()!;
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Szerintem ez nem egy szám.");
+                }
             }
             while (num is null);
-            return 0;
+            Console.ResetColor();
+            return num.Value;
         }
-        static double DoubleBeker(string text)
+ 
+        static double AskForDouble(string text)
         {
-            Console.Write(text);
-            string inp = Console.ReadLine()!;
 
             double? num = null;
             do
             {
+                Console.Write(text);
+                string inp = Console.ReadLine()!;
                 try
                 {
                     num = double.Parse(inp);
-                    return (double)num;
                 }
-                catch (Exception)
+                catch (OverflowException)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Valami nem jó itt, gondold át!");
+                    Console.WriteLine("Nagy szám ez, nem tudok vele mit kezdeni...");
                 }
-                Console.ForegroundColor = ConsoleColor.White;
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Szerintem ez nem egy szám.");
+                }
                 Console.Write(text);
                 inp = Console.ReadLine()!;
             }
             while (num is null);
-            return 0;
+            Console.ResetColor();
+            return num.Value;
+
         }
         static void Main(string[] args)
         {
-            Feladat14();
+            F15();
         }
     }
 }
